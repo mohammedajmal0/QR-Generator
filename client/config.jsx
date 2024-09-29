f8ccd5cbc26b7f825f9f7fcdd02a48b306147c58
Arfaz Ahmed <108565133+Arfazappu@users.noreply.github.com>
2024-09-29 20:39:13 +0530

    Update config.jsx

--- client/config.jsx
+++ client/config.jsx
@@ -1,1 +1,2 @@
-export const FE_URL = 'https://cryptocheck-proto-1.onrender.com';
+export const BASE_URL = 'https://cryptocheck-proto-1.onrender.com';
+export const FE_URL = 'https://feat-qrverify--cryptoqrverify.netlify.app';

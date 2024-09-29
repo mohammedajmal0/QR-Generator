a839aaef22a9acb1faea6bafd521998c855e90b7
ajmal-g4t <ajmalg4t@grow4tech.com>
2024-09-29 21:25:52 +0300

    changes for issuedDate,RegNumber,Detailsnotfound

--- client/config.jsx
+++ client/config.jsx
@@ -1,3 +1,3 @@
-// export const BASE_URL = 'https://cryptocheck-proto-1.onrender.com';
-export const BASE_URL = 'http://localhost:9098';
+export const BASE_URL = 'https://cryptocheck-proto-1.onrender.com';
+// export const BASE_URL = 'http://localhost:9098';
 export const FE_URL = 'https://cryptoqrverify.netlify.app';

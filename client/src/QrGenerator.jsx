e42cb7284a8752d25ff062fd8e5d8ef9656e4f90
ajmal-g4t <ajmalg4t@grow4tech.com>
2024-09-30 14:13:35 +0300

    added sample docs

--- client/src/QrGenerator.jsx
+++ client/src/QrGenerator.jsx
@@ -13,6 +13,28 @@ import Certificate2 from "./Certificate2";
 //const FE_URL="https://qr-generator-tvao-cw8vznivn-mohammed-ajmals-projects-95362c99.vercel.app"
 // const FE_URL="http://localhost:5173"
 
+const filePaths={
+  certificate:"./assets/sample1.xlsx",
+  bankDocument : "./assets/sample2.xlsx"
+}
+
+const requiredParams = {
+  certificate: [
+    { label: "Name", example: "John Doe" },
+    { label: "Course", example: "Bachelor of Science" },
+    { label: "Date", example: "01-01-2024" },
+    { label: "Register Number", example: "123456" }
+  ],
+  bankDocument: [
+    { label: "Account Holder Name", example: "Jane Smith" },
+    { label: "Bank Name", example: "Bank of America" },
+    { label: "Cheque Number", example: "789654123" },
+    { label: "Issue Date", example: "10-09-2024" }
+  ]
+};
+
+
+
 const QrGenerator = () => {
   const [excelData, setExcelData] = useState([]);
   const [documentType, setDocumentType] = useState("certificate");
@@ -20,57 +42,11 @@ const QrGenerator = () => {
   const [currentData, setCurrentData] = useState(null);
   const [generatedCertificates, setGeneratedCertificates] = useState([]);
   const [generatedCerificateData,setGeneratedCertificateData]=useState([])
+  const [isUploading, setIsUploading] = useState(false); // For loading state
+  const [isUploadDisabled, setIsUploadDisabled] = useState(false); // For disabling the button
+  const [alertMessage, setAlertMessage] = useState("");
 
-  // Handle Excel file upload
-//   const handleFileUpload = (e) => {
-//     const file = e.target.files[0];
-//     const reader = new FileReader();
-
-//     reader.onload = (event) => {
-//       const data = new Uint8Array(event.target.result);
-//       const workbook = XLSX.read(data, { type: "array" });
-//       const sheetName = workbook.SheetNames[0];
-//       const worksheet = workbook.Sheets[sheetName];
-//       const jsonData = XLSX.utils.sheet_to_json(worksheet);
-
-//     //   const parsedData = jsonData.map((row) => {
-//     //     const parsedRow = { ...row }; // Create a copy of the row
-    
-//     //     // Loop through each key in the row
-//     //     for (const key in parsedRow) {
-//     //       if (parsedRow.hasOwnProperty(key)) {
-//     //         const value = parsedRow[key];
-            
-//     //         // If the value is an Excel date serial number, convert it
-//     //         if (isExcelDate(value)) {
-//     //           parsedRow[key] = excelDateToJSDate(value).toLocaleDateString("en-GB", {
-//     //             day: "2-digit",
-//     //             month: "short",
-//     //             year: "numeric"
-//     //           }); // Convert to a readable date format
-//     //         }
-//     //       }
-//     //     }
-    
-//     //     return parsedRow;
-//     //   });
-
-//     //   console.log(parsedData)
-
-//       // Set Excel data
-//       setExcelData(jsonData);
-//     //   setExcelData(parsedData);
-
-//       // Check if there is data in the Excel file
-//       if (jsonData.length > 0) {
-//         setCurrentData(jsonData[0]);
-//       } else {
-//         alert("No data found in the uploaded Excel file.");
-//       }
-//     };
-
-//     reader.readAsArrayBuffer(file);
-//   };
+  
 const handleFileUpload = (e) => {
     const file = e.target.files[0];
     const reader = new FileReader();
@@ -191,6 +167,7 @@ const handleFileUpload = (e) => {
   
       const result = await response.json();
       console.log("Data uploaded successfully:", result);
+      
     } catch (error) {
       console.error("Error uploading data to backend:", error);
     }
@@ -205,51 +182,69 @@ const handleFileUpload = (e) => {
   
 
   const uploadCertificates = async (certificates) => {
-    await uploadGeneratedData(generatedCerificateData)
-    certificates.forEach((cert) => {
-      // Capture the certificate layout as an image using html2canvas
-      html2canvas(document.getElementById(`cert-${cert.QrId}`), {
-        scale: 2,
-      }).then((canvas) => {
-        const imgData = canvas.toDataURL("image/png");
-
-        const pdf = new jsPDF({
-          orientation: "landscape",
-          unit: "px",
-          format: [canvas.width, canvas.height + 100],
-        });
-
-        // Add the image to the PDF
-        pdf.addImage(imgData, "PNG", 0, 50, canvas.width, canvas.height, undefined, "FAST");//FAST compress the pdf
-        // pdf.save(`${cert.RegisterNumber}.pdf`)
-
-        // Convert the PDF to a Blob
-        const pdfBlob = pdf.output("blob");
-
-        // Prepare form data for the file upload
-        const formData = new FormData();
-        formData.append("file", pdfBlob, `${cert.RegisterNumber}.pdf`);
-        formData.append("fileType", "application/pdf");
-
-        // Upload the certificate PDF to the server
-        fetch(`${BASE_URL}/api/upload?fileName=${cert.RegisterNumber}`, {
-          method: "POST",
-          body: formData,
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            console.log("Certificate uploaded successfully:", data);
-          })
-          .catch((error) => {
-            console.error("Error uploading certificate:", error);
+    setIsUploading(true); 
+    setAlertMessage(""); 
+    try{
+      await uploadGeneratedData(generatedCerificateData)
+      certificates.forEach((cert) => {
+        // Capture the certificate layout as an image using html2canvas
+        html2canvas(document.getElementById(`cert-${cert.QrId}`), {
+          scale: 2,
+        }).then((canvas) => {
+          const imgData = canvas.toDataURL("image/png");
+  
+          const pdf = new jsPDF({
+            orientation: "landscape",
+            unit: "px",
+            format: [canvas.width, canvas.height + 100],
           });
+  
+          // Add the image to the PDF
+          pdf.addImage(imgData, "PNG", 0, 50, canvas.width, canvas.height, undefined, "FAST");//FAST compress the pdf
+          // pdf.save(`${cert.RegisterNumber}.pdf`)
+  
+          // Convert the PDF to a Blob
+          const pdfBlob = pdf.output("blob");
+  
+          // Prepare form data for the file upload
+          const formData = new FormData();
+          formData.append("file", pdfBlob, `${cert.RegisterNumber}.pdf`);
+          formData.append("fileType", "application/pdf");
+  
+          // Upload the certificate PDF to the server
+          fetch(`${BASE_URL}/api/upload?fileName=${cert.RegisterNumber}`, {
+            method: "POST",
+            body: formData,
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log("Certificate uploaded successfully:", data);
+            })
+            .catch((error) => {
+              console.error("Error uploading certificate:", error);
+            });
+        });
       });
-    });
+      setAlertMessage("Data uploaded successfully!");
+      setIsUploadDisabled(true);
+      setTimeout(() => {
+        setAlertMessage("");
+      }, 3000);
+    }
+    catch(error){
+      console.error("Error uploading certificates:", error);
+    }
+    finally{
+      setIsUploading(false)
+    }
+     
   };
+  const selectedFile=filePaths[documentType]
+const selectedParams = requiredParams[documentType];
 
   return (
     <div className="App">
-
+      {alertMessage && <div className="alert alert-success">{alertMessage}</div>}
         <h1 className="app-title">QR Code Generator</h1>
 
 
@@ -269,12 +264,29 @@ const handleFileUpload = (e) => {
           className="document-select"
         >
           <option value="certificate">University of Higher Learning-Certificate</option>
-          <option value="certificate2">Bank of America-Cheque</option>
+          <option value="bankDocument">Bank of America-Cheque</option>
         </select>
+
+       
 
         <button onClick={handleGenerate} className="generate-button">
           Generate
         </button>
+      </div>
+
+      <a href={selectedFile} download className="download-button">
+        Download Sample Excel
+        </a>
+
+        <div className="parameters-box">
+        <h3>Required Parameters</h3>
+        <ul>
+          {selectedParams.map((param, index) => (
+            <li key={index}>
+              <strong>{param.label}:</strong> <em>{param.example}</em>
+            </li>
+          ))}
+        </ul>
       </div>
 
       {/* {currentData && qrCodeUrl && (
@@ -287,13 +299,16 @@ const handleFileUpload = (e) => {
           />
         </>
       )} */}
-
+    {/* {successMessage && <p className="success-message">{successMessage}</p>} */}
       {generatedCertificates.length > 0 ? (
         <button
           onClick={() => uploadCertificates(generatedCertificates)}
-          className="upload-button generate-button"
+          className={`upload-button generate-button ${
+            isUploading || isUploadDisabled ? "disabled" : ""
+          }`}
+          disabled={isUploadDisabled || isUploading}
         >
-          Upload All Certificates
+          {isUploading ? "Loading..." : "Upload All Certificates"}
         </button>
       ) : null}
 
